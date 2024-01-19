b27486bc82f65c6c7679f48e759424779f4c725a
therealanuj <anujchaudharybst@gmail.com>
2024-01-20 00:52:42 +0530

    reverse

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -35,8 +35,8 @@ function Navbar() {
     ]
 
     return (
-        <header className='lg:px-24 px-4 bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
-            <nav className={`py-4  ${isSticky ? "sticky top-0 right-0 left-0 bg-blue-300" : ""}`}>
+        <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
+            <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 right-0 left-0 bg-blue-300" : ""}`}>
                 <div className='flex justify-between items-center text-base gap-8'>
                     <Link to="/" className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className="inline-block" />Books</Link>
                     <ul className='md:flex space-x-12 hidden'>
@@ -44,9 +44,9 @@ function Navbar() {
                             <Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>{link}</Link>
                         )}
                     </ul>
-                    {/* <div className='space-x-12 hidden lg:flex items-center'>
+                    <div className='space-x-12 hidden lg:flex items-center'>
                         <button><FaBarsStaggered className="w-5 hover:text-blue-700" /></button>
-                    </div> */}
+                    </div>
                     <div className='md:hidden'>
                         <button onClick={toggleMenu} className='text-black focus:outline-none'>{isMenuOpen ? <FaXmark className="h-5 w-5 text-black" /> : <FaBarsStaggered className="h-5 w-5 text-black" />}</button>
                     </div>
